c57529b778e0fdc7d7fa1589d0b94340a2f9c992
Hillius Ettinoffe <hillius.ettinoffe@gmail.com>
2013-09-14 11:55:44 -0700

    Renaming previous files to match new project name

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>StockTileJS</title>
+	<title>StockTil.es - Visualize your investments</title>
 	<meta charset="utf-8" />
 	<meta name="description" content="Quick Stock Monitoring" />
 	<meta name="author" content="Ashton Paul" />
@@ -10,10 +10,10 @@
 	<link href='http://fonts.googleapis.com/css?family=Maven+Pro:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" type="text/css" href="css/quote_widget.css">
+	<link rel="stylesheet" type="text/css" href="css/stocktiles.css">
 	
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-	<script type="text/javascript" src="js/quote_widget.js"></script>
+	<script type="text/javascript" src="js/stocktiles.js"></script>
 </head>
 
 <body>
